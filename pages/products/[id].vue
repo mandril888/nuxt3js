087f844bb9c3d7e73690b5c2d4009f3557cb6bad
Arturo Rubio <arubio@seocom.agency>
2023-02-20 16:51:49 +0100

    route parameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Product details:</p>
+        <ul>
+            <li>ID: {{ id }}</li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style lang="scss" scoped>
+
+</style>
